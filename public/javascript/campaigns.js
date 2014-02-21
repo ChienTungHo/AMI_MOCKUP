c67c82f1f53c33147ec0f2cabb37d3179560d4bb
tongtong <ho.tongtong@gmail.com>
2014-02-21 16:10:00 +0800

    Implement basic template and navbar

---
+++ public/javascript/campaigns.js
@@ -0,0 +1,29 @@
+
+function page_render(page_name){
+	// Do something when page switching
+	console.log(page_name);
+}
+
+function navbar_click_event(e){
+	// This function will be triggered when the navbar been clicked
+	
+	// Clear the main content
+	document.getElementById('main_info').innerHTML = "";
+
+	// Check whether the click item is DOM item 
+	for (var i = 0 ; i < this.children.length ; i++){
+	
+		if(this.children[i].children[0] == e.target){
+			$(this.children[i]).addClass("active");
+			page_render($(this.children[i]).attr('page-name'));
+		}
+		else{
+			$(this.children[i]).removeClass("active");
+		}
+	}
+}
+
+$(document).ready(function(){
+	document.getElementById('navbar-control').addEventListener('click',navbar_click_event, true);
+	
+});
