b8c5bb5738435c03d21e13caaa8730c1a0bbf1b9
tongtong <ho.tongtong@gmail.com>
2014-02-23 19:51:47 +0800

    fix bug in accordion and add comment

--- public/javascript/campaigns.js
+++ public/javascript/campaigns.js
@@ -37,6 +37,8 @@ function select_bar_click_event(){
 		if($(select_content.children[i]).attr('id') == $(this).attr('module-name')){
 			$(select_options.children[i]).addClass("selection_active");
 
+			// Remove the next selector top border. If we don't remove the border then 
+			// the bottom of the selected option will have one line there
 			if(i < select_content.children.length - 1){
 				$(select_options.children[i+1]).addClass("remove_top_border");
 			}
@@ -55,25 +57,27 @@ function select_bar_click_event(){
 	}
 }
 
-function accordion_bar_click_event(){
+function accordion_bar_click_event(e){
 	// This function will be triggered when the accordion link been clicked
 
 	var accordion_headers = document.getElementById("accordion");
 	var selected_headers = $(this.getElementsByTagName("a")[0]).attr("target-name");
 
-	for(var i = 0 ; i < accordion_headers.children.length ; i++){
-		
-		console.log(accordion_headers.children.length);
+	if(e.target.tagName != "A" && e.target.parentNode.tagName != "A")
+		return;
 
-		console.log($(accordion_headers.children[i].children[0]).attr('id'));
-		console.log(selected_headers);
+	for(var i = 0 ; i < accordion_headers.children.length ; i++){
+
 		if($(accordion_headers.children[i].children[0]).attr('id') == selected_headers){
+			// Check the panel has been selected or not
 			if($(accordion_headers.children[i].children[0]).hasClass("panel_heading_selected")){
+				// has been selected before then switch the blue selected status to the original one
 				$(accordion_headers.children[i].children[0]).removeClass("panel_heading_selected");
 				$(accordion_headers.children[i].children[0]).find("a").find(".glyphicon").first().addClass("hide");
 				$(accordion_headers.children[i].children[0]).find("a").find(".glyphicon").last().removeClass("hide");
 			}
 			else{
+				// has not been selected before then switch the original status to the selected status
 				$(accordion_headers.children[i].children[0]).addClass("panel_heading_selected");
 				$(accordion_headers.children[i].children[0]).find("a").find(".glyphicon").first().removeClass("hide");
 				$(accordion_headers.children[i].children[0]).find("a").find(".glyphicon").last().addClass("hide");
